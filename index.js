42461e6b7c9a5adcc601ad140d1e24fc5c75d06d
chivaata <Chivatavakol@gmail.com>
2021-04-10 18:02:44 +0430

    put only one button to toggle the menu

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-let openBtn = document.querySelector(".openSideNav");
-openBtn.addEventListener("click", () => {
-  showNav();
-});
-let closeBtn = document.querySelector(".closeBtn");
-closeBtn.addEventListener("click", () => {
-  hideNav();
-});
-function showNav() {
-  document.querySelector(".sideNav").style.width = "200px";
-  document.querySelector(".main-content").style.marginLeft = "200px";
-}
-function hideNav() {
-  document.querySelector(".sideNav").style.width = "0";
-  document.querySelector(".main-content").style.marginLeft = "0px";
+var menu = document.querySelector(".sideNav");
+var main = document.querySelector(".main-content");
+var btn = document.querySelector(".openSideNav");
+var open = false;
+function toggleMenu() {
+  if (!open) {
+    open = true;
+    menu.style.width = "200px";
+    main.style.marginLeft = "200px";
+    btn.innerHTML = "&lt";
+  } else if (open) {
+    open = false;
+    menu.style.width = "0";
+    main.style.marginLeft = "0";
+    btn.innerHTML = "&gt";
+  }
 }
