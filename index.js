a8016f4e3d8f26bc24e476deab5208dc5622bcde
chivaata <Chivatavakol@gmail.com>
2021-04-12 18:35:56 +0430

    some style added.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+///////////////screen sizes///////////////
+
+var mobile = window.matchMedia("(max-width:425px;)");
+var tablet = window.matchMedia("(max-width:768px;)");
+var laptop = window.matchMedia("(max-width:1440px;)");
+var bigScreen = window.matchMedia("(min-width:1441px;)");
+
+//////////////////////////////////////////
 var menu = document.querySelector(".sideNav");
 var main = document.querySelector(".main-content");
 var btn = document.querySelector(".openSideNav");
 var open = false;
+
+////////////////functions/////////////////
 function toggleMenu() {
+  console.log("click");
   if (!open) {
     open = true;
-    menu.style.width = "200px";
-    main.style.marginLeft = "200px";
     btn.innerHTML = "&lt";
+    menu.style.width = "160px";
+    main.style.marginLeft = "160px";
   } else if (open) {
     open = false;
+    btn.innerHTML = "&gt";
     menu.style.width = "0";
     main.style.marginLeft = "0";
-    btn.innerHTML = "&gt";
   }
 }
